ec2fba0a6134bc48e812a26d38117a07d8d11923
DhruvMakani <124004385+DhruvMakani@users.noreply.github.com>
2024-12-03 22:16:01 -0500

    API Key for accessing

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "65ca0f7800e735990f1aa43561f4a035"; // API Key for accessing the OpenWeatherMap API
+const form = document.getElementById("weatherForm");
+const weatherOutput = document.getElementById("weatherOutput");
